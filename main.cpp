c4150560a6a02021374557d04e4b1c97c55d0c02
AstariaStudio <github@astaria.dev>
2025-10-29 04:12:34 -0700

    1st Commit of actually work

--- main.cpp
+++ main.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
+
+void debugmode(string state, string mode, string location) {
+    cout << "State: " << state  << endl;
+    cout << "Mode: " << mode  << endl;
+    cout << "Location: " << location << endl;
+    cout << endl;
+}
+
+void main_menu() {
+    system("cls");
+
+    cout << setw(24) << setfill('*') << " Main Menu ";
+}
+
 int main() {
     bool game_over = false;
     bool game_active = true;
+    string game_mode = "menu", game_state = "main_menu", game_location = "main menu";
 
-    cout << R"(____ ___  ____ ____ ____    ____ _  _ ____ ____ ____ ____ _  _ ____ _   _
-[__  |__] |__| |    |___    |___ |\/| |___ |__/ | __ |___ |\ | |     \_/
-___] |    |  | |___ |___    |___ |  | |___ |  \ |__] |___ | \| |___   |  )" << endl;
+    //notes on what to implement
+    // function that when game start it opens saved info
+    //function that displays gui secretly, rooms as functions
 
-    int counter = 0;
-    while(game_active) {
-        cout << "Main Menu" << endl << endl << endl;
-        counter++;
 
-        if(counter == 2000) {break;}
-    }
+
+
+    main_menu();
 
     return 0;
 }
+
+void GameOver() {}
+void GameStart() {}
